b15df109eac9a50ece9ffe1beab1d110a49b3f6e
martinsetaro <lordsesony@gmail.com>
2022-04-26 00:43:30 -0300

    arreglo en itemlistcontainer

--- src/components/Main/ItemList.js
+++ src/components/Main/ItemList.js
@@ -1,49 +1,14 @@
-import React,{useState,useEffect} from 'react'
-import Peliculas from '../../mock/Peliculas'
+
 import Item from './Item'
 
-const ItemList = () => {
-const [peliculas,setPeliculas] = useState([])
-
-// const promesa = new Promise((resolve) => {
-    
-//         resolve(Peliculas)
-//         console.log("Descarga exitosa")
-    
-// })
-    
-    
-    
-
-useEffect(()=>{
-
-    const promesa = new Promise((resolve) => {
-          setTimeout(()=>{
-               resolve(Peliculas)
-                console.log("Descarga exitosa")
-                
+const ItemList = ({nombre,imagen,info}) => {
 
 
-          },2000)
 
-
-    })
-  
-   
-
-    setTimeout(()=>{
-setPeliculas(Peliculas)
-    },2000);
-
-
-},[]);
   return (
-      <div className='listadoItems'>
-    {peliculas.map(pelicula =>(
-        <Item nombre={pelicula.nombre} imagen={pelicula.imagen} info={pelicula.info} key={pelicula.id}/>
-    ))}
-   
-</div>
+    <div className="cardPeliculas">
+      <Item nombre={nombre} imagen={imagen} info={info}/>
+    </div>
   )
 }
 
--- src/components/Main/ItemListContainer.js
+++ src/components/Main/ItemListContainer.js
@@ -1,20 +1,49 @@
 import '../Main/itemList.scss';
 import ItemList from './ItemList';
+import React,{useState,useEffect} from 'react'
+import Peliculas from '../../mock/Peliculas'
 
 
 
 
-const ItemListContainer = (props) => {
+const ItemListContainer = () => {
+
+
+  const [peliculas,setPeliculas] = useState([])
+
+
+  useEffect(()=>{
+  
+      const promesa = new Promise((resolve) => {
+            setTimeout(()=>{
+                 resolve(Peliculas)
+                  console.log("Descarga exitosa")
+                 },2000)
+  })
+    
+     
+  
+      setTimeout(()=>{
+  setPeliculas(Peliculas)
+      },2000);
+  
+  
+  },[]);
+
+
+
+
+
+
   return (
       
       <div className="listContainer">
-   <h2>{props.name}</h2>
-  <ItemList/>
+    {peliculas.map(pelicula =>
+        <ItemList nombre={pelicula.nombre} imagen={pelicula.imagen} info={pelicula.info} key={pelicula.id}/>
+    )}
    
-   </div>
-   
-   
+</div>
   )
 }
-
+      
 export default ItemListContainer;
